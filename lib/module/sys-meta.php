7a61a8ab664b75637dd571b252141c0f1817f8e7
isack5268 <57671827+isack5268@users.noreply.github.com>
2021-03-12 17:35:12 +0300

    very stupid fixes

--- lib/module/sys-meta.php
+++ lib/module/sys-meta.php
@@ -3,18 +3,18 @@
 <link rel="icon" type="image/png" href="/img/meta/favicon/96.png" sizes="96x96"/>
 <link rel="icon" type="image/png" href="/img/meta/favicon/128.png" sizes="128x128"/>
 <link rel="icon" type="image/png" href="/img/meta/favicon/196.png" sizes="196x196"/>
-<link rel="apple-touch-icon" href="/img/meta/mobile/57.png" sizes="57x57"/>
-<link rel="apple-touch-icon" href="/img/meta/mobile/60.png" sizes="60x60"/>
-<link rel="apple-touch-icon" href="/img/meta/mobile/72.png" sizes="72x72"/>
-<link rel="apple-touch-icon" href="/img/meta/mobile/76.png" sizes="76x76"/>
-<link rel="apple-touch-icon" href="/img/meta/mobile/114.png" sizes="114x114"/>
-<link rel="apple-touch-icon" href="/img/meta/mobile/120.png" sizes="120x120"/>
-<link rel="apple-touch-icon" href="/img/meta/mobile/144.png" sizes="144x144"/>
-<link rel="apple-touch-icon" href="/img/meta/mobile/152.png" sizes="152x152"/>
+<link rel="apple-touch-icon" href="/img/meta/mobile/57.png?v=2" sizes="57x57"/>
+<link rel="apple-touch-icon" href="/img/meta/mobile/60.png?v=2" sizes="60x60"/>
+<link rel="apple-touch-icon" href="/img/meta/mobile/72.png?v=2" sizes="72x72"/>
+<link rel="apple-touch-icon" href="/img/meta/mobile/76.png?v=2" sizes="76x76"/>
+<link rel="apple-touch-icon" href="/img/meta/mobile/114.png?v=2" sizes="114x114"/>
+<link rel="apple-touch-icon" href="/img/meta/mobile/120.png?v=2" sizes="120x120"/>
+<link rel="apple-touch-icon" href="/img/meta/mobile/144.png?v=2" sizes="144x144"/>
+<link rel="apple-touch-icon" href="/img/meta/mobile/152.png?v=2" sizes="152x152"/>
 <link rel="manifest" href="/lib/js/manifest.json">
 <meta name="theme-color" content="#4e34c9">
 <meta name=viewport content="width=device-width, initial-scale=.6"/>
-<meta property="og:locale" content="en_US"/>
+<meta property="og:locale" content="ru_Ru"/>
 <meta property="og:url" content="https://pancakefaces.ru/"/>
 <meta property="og:type" content="website"/>
 <meta property="og:title" content="Pancake Faces project"/>
