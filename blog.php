58953e6942221005ce18594facea88ce80dcb4ad
isack5268 <57671827+isack5268@users.noreply.github.com>
2021-03-22 21:25:24 +0300

    added blog sidebar's anchors

--- blog.php
+++ blog.php
@@ -13,6 +13,7 @@
 </head>
 <body>
 <?php include 'lib/module/sys-php.php';?>
+<?php include 'lib/module/ui-sidebar-blog.php';?>
 <div class="page-con-content">
 	<div class="banner-con-container darkmode-header">
 		<div id="object-particles">
@@ -39,15 +40,22 @@
 			<div class='container-con-block darkmode-block'>
 				<div class='container-con-wrapper'>
 					<div class='container-tx1-block darkmode-txt'>
-						<h2>Заголовок статьи </h2>
+						<h2 id="1">Собран тестовый прототип HotPotato</h2>
 					</div>
 					<div class='container-tx2-block darkmode-txt'>
 						<p>
-						Текст </p>
+						Сейчас мы уже собрали тестовую модель игрушки. Вышло вполне играбельно, но это тестовый прототип только с базовыми функциями, чтобы возможно было играть.<br>
+						В процессе сборки возникла идея отказаться от идеи "картошки" и собрать всю конструкцию в компактный кубик и избавиться от кнопок и выклчателей, заменив их на систему потрясывания (условно, как в 8ball)</p>
 					</div>
                     <div class='blog-img'>
-                    <img src="/img/graphics/landing/index.png">
+                    	<img src="https://sun9-31.userapi.com/impg/fixsKwxUPze5Wmo85dMoPf9fBL6FyMGxGj3AnA/-ZXGGKwQwog.jpg?size=200x267&quality=96&sign=3b67813409a5a21b75a2cae7a9648bf2&type=album" hspace="5" vspace="5">
+						<img src="https://sun9-11.userapi.com/impg/wu4ip2qhay-TvKFoJOZqAb8seE5t0PtAl0MY0g/ckCgUL9gmK0.jpg?size=200x267&quality=96&sign=c2cabddf1d87400bd83ed2979b2c255d&type=album" hspace="5" vspace="5">
+						<img src="https://sun9-2.userapi.com/impg/3PMKUzXiKX0tuH7FpmGhZRV_dZQ0LW2v-MZ1FQ/Y2iK-LGMUho.jpg?size=200x267&quality=96&sign=bc45f221d2cc9db60edb87eaf8917211&type=album" hspace="5" vspace="5">
+						<img src="https://sun9-51.userapi.com/impg/BAE-zfR8X8RkBuLGBI5VvEDiA-Atia57SN1i9A/RwUwO36-IXQ.jpg?size=200x267&quality=96&sign=9b73c5f529dbd5cc393c1b98a8ebcc7d&type=album" hspace="5" vspace="5">
                     </div>
+					<div class='container-tx3-block darkmode-txt'>
+						<p>21.03.2021</p>
+					</div>
 				</div>
 			</div>
 		</div>
