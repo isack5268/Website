a22b10abf2cc7f39f682ad9f9f2b38756bcb4772
isack5268 <57671827+isack5268@users.noreply.github.com>
2021-04-14 00:16:57 +0300

    Disclaimer edited for this site

--- disclaimer.php
+++ disclaimer.php
@@ -3,10 +3,10 @@
 <head>
 <title>Дисклеймер</title>
 <meta charset="utf-8">
-<meta name="description" content="RPCS3 is a multi-platform open-source Sony PlayStation 3 emulator and debugger written in C++ for Windows, Linux and BSD.">
-<meta name="keywords" content="rpcs3, playstation, playstation 3, ps3, emulator, debugger, windows, linux, bsd, open source, nekotekina, kd11, disclaimer">
-<meta name="author" content="RPCS3">
-<meta name="copyright" content="RPCS3">
+<meta name="description" content="Pancake Faces project ялвляется open source проектом, предоставляющим в открытый доступ свои разработки в игровом движке Unity.">
+<meta name="keywords" content="pancake faces, unity, open source, разработки, development, 2d, windows, project, disclaimer, дисклеймер">
+<meta name="author" content="Pancake Faces">
+<meta name="copyright" content="Pancake Faces">
 <?php include 'lib/module/sys-meta.php';?>
 <?php include 'lib/module/sys-css.php';?>
 <?php include 'lib/module/sys-js.php';?>
@@ -25,11 +25,11 @@
 		</div>
 		<div class='banner-con-title fade-up-onstart'>
 			<div class='banner-tx1-title fade-up-onstart pulsate'>
-				<h1>Disclaimer</h1>
+				<h1>Дисклеймер</h1>
 			</div>
 			<div class='banner-tx2-title fade-up-onstart'>
 				<p>
-					 A legal disclaimer for RPCS3 and its developments
+					 Правовая оговорка для Pancake Faces project и её разработок
 				</p>
 			</div>
 		</div>
@@ -39,13 +39,11 @@
 			<div class="container-con-block darkmode-block">
 				<div class='container-con-wrapper'>
 					<div class='container-tx1-block darkmode-txt'>
-						<h2>Trademarks and Copyright</h2>
+						<h2>Товарные знаки и авторские права</h2>
 					</div>
 					<div class='container-tx2-block darkmode-txt'>
 						<p>
-							 All trademarks and copyright-written content found on this website belong to their respective owners. The RPCS3 team is in no way affiliated with Sony or PlayStation. <br>
-							<br>
-							 The "PlayStation logo", "PlayStation 3 logo", "PlayStation 4 logo", "PlayStation Portable logo", "PlayStation Vita logo", "PlayStation Move logo", "PlayStation Network logo", "PlayStation Store logo", "PlayStation Plus logo", "Sony logo", "Sony Computer Entertainment logo" and their aforementioned names are registered trademarks of Sony Computer Entertainment Inc. "Sony Entertainment Network" is a trademark of Sony Corporation. Library programs are copyright Sony Interactive Entertainment Inc.
+							 Все товарные знаки и авторские права, содержащиеся на этом сайте, принадлежат их соответствующим владельцам.
 						</p>
 					</div>
 				</div>
@@ -53,11 +51,11 @@
 			<div class="container-con-block darkmode-block">
 				<div class='container-con-wrapper'>
 					<div class='container-tx1-block darkmode-txt'>
-						<h2>Intellectual Properties</h2>
+						<h2>Лицензирование с открытым ключом</h2>
 					</div>
 					<div class='container-tx2-block darkmode-txt'>
 						<p>
-							 This website displays various trademarks and copy-written content such as videogame logos, company logos, videogame names and company names. These different trademarks and copy-written content do not belong to us and are properties of their respective owners. This website is in no way affiliated with said companies or trademarks. This website’s only intention is to spread the word and endorse the content displayed.
+							 Согласно лицензии, вы можете использовать наш сайт и его исходный код для любых целей, но распространение требует, чтобы исходный код был выпущен и дана атрибуция. Для получения более подробной информации о том, как работает система общих публичных лицензий, пожалуйста, обратитесь к нам.
 						</p>
 					</div>
 				</div>
@@ -65,11 +63,15 @@
 			<div class="container-con-block darkmode-block">
 				<div class='container-con-wrapper'>
 					<div class='container-tx1-block darkmode-txt'>
-						<h2>Open-source Licensing</h2>
+						<h2>Заявление о пиратстве и незаконных действиях</h2>
 					</div>
 					<div class='container-tx2-block darkmode-txt'>
 						<p>
-							 RPCS3 uses the GNU General Public License Version 2 (June 1991). According to the license, you are welcome to use RPCS3 and its source code for any purpose, but distributing RPCS3 requires that the source code be released and attribution given. For more details on how the GNU General Public License system works, please refer to <a href="https://www.gnu.org/" target="_blank">GNU.org</a>
+							 <a href="http://pancakefaces.ru">Pancakefaces.ru</a> не предназначен для осуществления незаконной деятельности. Мы не поощряем пиратство и не допускаем его ни при каких обстоятельствах. Любые пользователи, говорящие о пиратстве после присоединения к серверу Discord, форумам или сообществу GitHub, получат административные меры, либо аккаунт будет забанен на время/навсегда. 
+							 <br>
+							 <br> Пожалуйста, найдите время, чтобы ознакомиться с законами об авторском праве и демпинге программного обеспечения для видеоигр и/или политикой вашей страны, прежде чем продолжить.
+							 <br>
+							 <br> Следуя этим инструкциям, вы будете делать это по своему усмотрению. Если вы будете следовать этим инструкциям вопреки местному законодательству, мы не будем нести ответственность за ваши действия.
 						</p>
 					</div>
 				</div>
@@ -77,11 +79,11 @@
 			<div class="container-con-block darkmode-block">
 				<div class='container-con-wrapper'>
 					<div class='container-tx1-block darkmode-txt'>
-						<h2>Sony Interactive Entertainment Proprietary Technologies</h2>
+						<h2>Разработка и экспериментирование</h2>
 					</div>
 					<div class='container-tx2-block darkmode-txt'>
 						<p>
-							 Library programs are copyright Sony Interactive Entertainment Inc. Licensed for play on the PlayStation 3 Computer Entertainment System or authorized PlayStation 3 format systems. For full terms and conditions see the official <a href="https://www.playstation.com/en-us/support/manuals/ps3/" target="_blank">PlayStation 3 user's manual</a>. The PlayStation 3 system is authorized and produced under license form Sony Interactive Entertainment Inc. Use is subject to the copyright laws and the terms and conditions of the user's license.
+							 Цель этого проекта-экспериментировать, исследовать и обучать на тему IT, которые могут быть выполнены на совместимых устройствах и операционных системах. Дополнительная информация была получена из различных источников в Интернете, которые включают, но не ограничиваются системной аппаратной и программной документацией.
 						</p>
 					</div>
 				</div>
@@ -89,39 +91,13 @@
 			<div class="container-con-block darkmode-block">
 				<div class='container-con-wrapper'>
 					<div class='container-tx1-block darkmode-txt'>
-						<h2>Statement on Piracy and Illegal Acts</h2>
+						<h2>Заявление Google Analytics</h2>
 					</div>
 					<div class='container-tx2-block darkmode-txt'>
 						<p>
-							 When dumping video game software, users are subject to country-specific software distribution laws. RPCS3 is not designed to enable illegal activity. We do not promote piracy nor do we allow it under any circumstances. Any users conversing about piracy upon joining the Discord server, forums or GitHub community will receive administrative action. Please take the time to review copyright and video game software dumping laws and/or policies for your country before proceeding.<br>
-							<br>
-							 By following these instructions, you will do so at your own discretion. Should you follow these instructions against your local law, we shall not be held responsible for your actions.
-						</p>
-					</div>
-				</div>
-			</div>
-			<div class="container-con-block darkmode-block">
-				<div class='container-con-wrapper'>
-					<div class='container-tx1-block darkmode-txt'>
-						<h2>Development and Experimentation</h2>
-					</div>
-					<div class='container-tx2-block darkmode-txt'>
-						<p>
-							 The goal of this project is to experiment, research, and educate on the topic of PlayStation 3 emulation that can be performed on compatible devices and operating systems. All information was obtained legally by purchasing PlayStation 3 hardware and software. Additional information was obtained from various sources on the internet that include but is not limited to system hardware and software documentation. Most of this information can be found on the <a href='http://www.psdevwiki.com/ps3/Main_Page' target="_blank">PlayStation 3 Developer Wiki.</a>
-						</p>
-					</div>
-				</div>
-			</div>
-			<div class="container-con-block darkmode-block">
-				<div class='container-con-wrapper'>
-					<div class='container-tx1-block darkmode-txt'>
-						<h2>Google Analytics Statement</h2>
-					</div>
-					<div class='container-tx2-block darkmode-txt'>
-						<p>
-							 This website uses Google Analytics, a web analytics service provided by Google, Inc. (“Google”). Google Analytics uses “cookies”, which are text files placed on your computer, to help the website analyze how users use the site. The information generated by the cookie about your use of the website (including your IP address) will be transmitted to and stored by Google on servers in the United States . Google will use this information for the purpose of evaluating your use of the website, compiling reports on website activity for website operators and providing other services relating to website activity and internet usage.<br>
-							<br>
-							 Google may also transfer this information to third parties where required to do so by law, or where such third parties process the information on Google’s behalf. Google will not associate your IP address with any other data held by Google. You may refuse the use of cookies by selecting the appropriate settings on your browser, however please note that if you do this you may not be able to use the full functionality of this website. By using this website, you consent to the processing of data about you by Google in the manner and for the purposes set out above.
+							 Google Analytics использует ”cookie", которые представляют собой текстовые файлы, размещенные на вашем компьютере, чтобы помочь веб-сайту проанализировать, как пользователи используют сайт. Информация, генерируемая файлом cookie об использовании вами веб-сайта (включая ваш IP-адрес), будет передаваться и храниться компанией Google на серверах в Соединенных Штатах . Google будет использовать эту информацию для оценки вашего использования веб-сайта, составления отчетов о деятельности веб-сайта для операторов веб-сайтов и предоставления других услуг, связанных с деятельностью веб-сайта и использованием Интернета.
+							 <br>
+							 <br> Google также может передавать эту информацию третьим лицам, если это требуется по закону или если такие третьи лица обрабатывают информацию от имени Google. Google не будет связывать ваш IP-адрес с какими-либо другими данными, хранящимися в Google. Вы можете отказаться от использования файлов cookie, выбрав соответствующие настройки в своем браузере, по отношению к <a href="http://pancakefaces.ru ">Pancake Faces project</a>.
 						</p>
 					</div>
 				</div>
