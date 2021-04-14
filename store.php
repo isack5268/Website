613e17b3c85d290d5d1137761cd49ac49ceca077
isack5268 <57671827+isack5268@users.noreply.github.com>
2021-04-14 23:35:59 +0300

    added store page

---
+++ store.php
@@ -0,0 +1,58 @@
+<!doctype html>
+<html lang="en-US">
+<head>
+<title>Магазин</title>
+<meta charset="utf-8">
+<meta name="description" content="Pancake Faces project ялвляется open source проектом, предоставляющим в открытый доступ свои разработки в игровом движке Unity.">
+<meta name="keywords" content="pancake faces, unity, open source, разработки, development, 2d, windows, project, store, магазин">
+<meta name="author" content="Pancake Faces">
+<meta name="copyright" content="Pancake Faces">
+<?php include 'lib/module/sys-meta.php';?>
+<?php include 'lib/module/sys-css.php';?>
+<?php include 'lib/module/sys-js.php';?>
+<script type="text/javascript" src="https://vk.com/js/api/openapi.js?169"></script>
+</head>
+<body>
+<?php include 'lib/module/sys-php.php';?>
+<div class="page-con-content">
+	<div class="banner-con-container darkmode-header">
+		<div id="object-particles">
+		</div>
+		<div class="wavebar-con-container">
+			<div class="wavebar-con-wrap">
+			  <div class="wavebar-svg-object"></div>
+			  <div class="wavebar-svg-object"></div>
+			</div>
+		</div>
+		<div class='banner-con-title fade-up-onstart'>
+			<div class='banner-tx1-title fade-up-onstart pulsate'>
+				<h1>Магазин</h1>
+			</div>
+			<div class='banner-tx2-title fade-up-onstart'>
+				<p>
+					 Тут будут находиться наши товары
+				</p>
+			</div>
+		</div>
+	</div>
+	<div class="page-con-container">
+		<div class="page-in-container">
+			<div class='container-con-block darkmode-block'>
+				<div class='container-con-wrapper'>
+                    <div id="my-store-58323061"></div>
+                    <div class="darkmode-txt">
+                        <script data-cfasync="false" type="text/javascript" src="https://app.ecwid.com/script.js?58323061&data_platform=code&data_date=2021-04-14" charset="utf-8"></script><script type="text/javascript"> xProductBrowser("categoriesPerRow=3","views=grid(20,3) list(60) table(60)","categoryView=grid","searchView=list","id=my-store-58323061");</script>
+                    </div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div id="vk_community_messages">
+		<script type="text/javascript">
+			VK.Widgets.CommunityMessages("vk_community_messages", 191834852, {tooltipButtonText: "Есть вопрос?"});
+		</script>
+	<div>
+	<?php include 'lib/module/ui-main-footer.php';?>
+</div>
+</body>
+</html>
